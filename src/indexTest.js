8553aafc5778f76176f18030244291199fd518d8
MaxitoMix <128189550+MaxitoMix@users.noreply.github.com>
2023-05-24 12:59:11 -0400

    ,

--- src/indexTest.js
+++ src/indexTest.js
@@ -1,24 +1,44 @@
+
 let projectList = [
-    {
-      name: 'Project1',
-      todos: ['Lorem ipsum dolor sit amet.', 'Ut enim ad minim veniam.', '19 packages are looking for funding.'],
-    },
-    {
-      name: 'Project2',
-      todos: ['Ut enim ad minim veniam.', 'Ut enim ad minim veniam.']
-    },
-    {
-      name: 'Project3',
-      todos: ['19 packages are looking for funding.']
-    }
-  ];
+  {
+    name: 'Project1',
+    todos: [
+      { text: 'Lorem ipsum dolor sit amet.', recordDate: '2023-05-20', dueDate: '2023-05-26' },
+      { text: 'Ut enim ad minim veniam.', recordDate: '2023-05-20', dueDate: '2023-05-27' },
+      { text: '19 packages are looking for funding.', recordDate: '2023-05-20', dueDate: '2023-05-28' }
+    ],
+    doing: [
+      { text: 'Lorem ipsum dolor sit amet.', recordDate: '2023-05-20', dueDate: '2023-05-26' },
+      { text: 'Ut enim ad minim veniam.', recordDate: '2023-05-20', dueDate: '2023-05-27' },
+    ],
+    done: [
+      { text: 'Lorem ipsum dolor sit amet.', recordDate: '2023-05-20', dueDate: '2023-05-26' },
+      { text: 'Ut enim ad minim veniam.', recordDate: '2023-05-20', dueDate: '2023-05-27' },
+    ],
+  },
+  {
+    name: 'Project2',
+    todos: [
+      { text: 'Ut enim ad minim veniam.', recordDate: '2023-05-20', dueDate: '2023-05-29' },
+      { text: 'Ut enim ad minim veniam.', recordDate: '2023-05-20', dueDate: '2023-05-30' }
+    ]
+  },
+  {
+    name: 'Project3',
+    todos: [
+      { text: '19 packages are looking for funding.', recordDate: '2023-05-20', dueDate: '2023-06-01' }
+    ]
+  }
+];
 
 //factory creat project
 function createProject(name, todos) {
-return {
+  return {
     name,
     todos,
-};
+    doing,
+    done
+  };
 }
 
 // Function to add a new project to the projectList
@@ -83,9 +103,36 @@ function moveTodo(projectListIndex, currentPos, newPos) {
       return;
     }
   
-    const [removedTodo] = todos.splice(currentPos, 1);
-    todos.splice(newPos, 0, removedTodo);
+    const [movedTodo] = todos.splice(currentPos, 1);
+    todos.splice(newPos, 0, movedTodo);
   }
+
+// Function to move a todo within a project from 'todos' to either 'doing' or 'done'
+function moveTodoToStatus(projectListIndex, todosIndex, newStatus) {
+    if (projectListIndex < 0 || projectListIndex >= projectList.length) {
+      console.log('Invalid project index.');
+      return;
+    }
+  
+    const projectSelection = projectList[projectListIndex];
+    const todos = projectSelection.todos;
+  
+    if (todosIndex < 0 || todosIndex >= todos.length) {
+      console.log('Invalid to-do index for the specified project.');
+      return;
+    }
+
+    if (!['doing', 'done'].includes(newStatus)) {
+      console.log('Invalid status. Choose either "doing" or "done".');
+      return;
+    }
+ 
+    const [movedTodo] = todos.splice(todosIndex, 1);
+    projectSelection[newStatus].push(movedTodo);
+}
+
 
 
 console.log(projectList)
+
+
